4fb9b9f12f3a3c6627f57399603a56bb9160535d
ChadCasey <gbchadd@gmail.com>
2024-02-28 19:27:31 -0700

    added images and flowerObject

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+const flowerObjects = {
+  Hellebore: {
+    img: "../images/Hellebore.png",
+    title: "Taking Care Of The Hellebore",
+    description: "",
+  },
+
+  Daylily: {
+    img: "../images/Daylily.png",
+    title: "Nurturing The Daylily",
+    description: "",
+  },
+
+  Daylily: {
+    img: "../images/Bleedingheart.png",
+    title: "Raising the Bleedingheart",
+    description: "",
+  },
+
+  Bloodroot: {
+    img: "../images/Bloodroot.png",
+    title: "Providing For The Bloodroot",
+    description: "",
+  },
+};
