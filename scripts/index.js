c0c2ea2497a3ddbc795dbd3b34ae27a14ab7d01b
ChadCasey <gbchadd@gmail.com>
2024-02-29 18:33:27 -0700

    button stuff and description for flowers

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,9 @@ const flowerObjects = {
   Hellebore: {
     img: "../images/Hellebore.png",
     title: "Taking Care Of The Hellebore",
-    description: `description: Helleborus belongs to a small genus of around 20 herbaceous native to Asia and Europe.
+    description: `Helleborus belongs to a small genus of around 20 Herbaceous native to Asia and Europe.
                   The genus was created by Carl Linnaeus in 1753 within volume one of his "Species Plantarum."
-                  It is believed that the name originated from the Ancient Greek word elléboros. Many hellebore
+                  It is believed that the name originated from the Ancient Greek word elléboros. Fun fact, many hellebore
                   species are considered toxic, and if ingested, have a burning acrid taste. This species can be planted
                   in the spring or fall, and does well in well drained, fertile soil.`,
   },
@@ -12,7 +12,11 @@ const flowerObjects = {
   Daylily: {
     img: "../images/Daylily.png",
     title: "Nurturing The Daylily",
-    description: "",
+    description: `The Daylily is a flowering plant in the genus Hemerocallis, a member of the Asphodelaceae
+      family, and is native to Asia. The name Daylily originates from the greek word hemera, meaning day, because of
+      how long each flower lasts and kallos, meaning beauty. The flower has long been planted by gardening enthusiast
+      and horticulturalist for their attractive flowers. Daylilies do best when planted in early to mid spring,
+      so plan accordingly if you wish to grow some of your own this season.`,
   },
 
   Bleedingheart: {
